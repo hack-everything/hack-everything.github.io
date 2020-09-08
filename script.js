166960909e491851371c2724dd6781d578384d87
SourFruit   <65507526+hack-everything@users.noreply.github.com>
2020-09-08 18:16:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+console.log('Loading code into memory........');
+console.log('Interrogating public IP.........');
+console.log('Interrogating local IP.........');
+
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+console.log('HACKED');
+console.log('      ');
+
+console.log(' You found Easter Egg #1 ');
+
+
+
+
+
+
+
