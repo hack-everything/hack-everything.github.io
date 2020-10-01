ab5c3c5879330937bb259d9d55cac21a7cf06eeb
SourFruit   <65507526+hack-everything@users.noreply.github.com>
2020-09-30 20:02:50 -0400

    Delete pi.js

--- pi.js
+++
@@ -1,5 +0,0 @@
-console.log('hey..... pssssst')
-console.log('Email us the code: CONSOLE3')
-console.log('To get 1 month of Pi Zero hosting for $3!')
-console.log('That\'s $1 per Pi, for a month!')
-
