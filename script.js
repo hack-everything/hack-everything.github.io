52b76931c0f624b564882af8ff49914225c35ea5
Sourfruit <65507526+hack-everything@users.noreply.github.com>
2020-10-20 10:39:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ console.log('      ');
 
 console.log(' You found Easter Egg #1 ');
 
-var randomcookie = Math.floor((Math.random() * 10044000) + 234456);
+var randomcookie = Math.floor((Math.random() * 10044234567890000) + 2);
 document.cookie = randomcookie;
 // fuck youuuuuuuu
 document.cookie = randomcookie;
