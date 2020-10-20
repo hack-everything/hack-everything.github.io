50be8e006b430331477582aa4e8041ff6128b7da
Sourfruit <65507526+hack-everything@users.noreply.github.com>
2020-10-19 20:25:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,27 +22,435 @@ console.log('      ');
 
 console.log(' You found Easter Egg #1 ');
 
-
-
-
-
-$(function() {
-    $(window).on('resize', function resize()  {
-        $(window).off('resize', resize);
-        setTimeout(function () {
-            var content = $('#content');
-            var top = (window.innerHeight - content.height()) / 2;
-            content.css('top', Math.max(0, top) + 'px');
-            $(window).on('resize', resize);
-        }, 50);
-    }).resize();
-});
-$(document).bind("contextmenu",function(e) {
-    e.preventDefault();
-});
-$(document).keydown(function(e){
-    if(e.which === 123){
-        return false;
-    }
-});
-// secret files haha owo somewhere idk help 
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
+var randomcookie = Math.floor((Math.random() * 100) + 1);
+document.cookie = randomcookie;
+// fuck youuuuuuuu
